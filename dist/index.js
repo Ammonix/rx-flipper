5ea91aef6fc5c3ffd6893c98767b806331bfd350
Joan Künzler <joan.kuenzler@raiffeisen.ch>
2018-04-27 13:36:26 +0200

    wip

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log(Math.pow(2, 4));
+const rxjs_1 = require("rxjs");
+const operators_1 = require("rxjs/operators");
+const fs = require("fs");
+// const bit$ = from([0, 0, 0, 1]);
+// bit$.pipe(map(i => !i)).subscribe(console.log);
+// const rs = fs.createReadStream('files/in/bin');
+// const data$ = fromEvent(rs, 'data').pipe(map((i: Buffer) => i.length));
+// data$.subscribe(console.log);
+// fs.open('files/in/bin', 'r', (error, fd) => {
+//   if (error) {
+//     console.error(error.message);
+//     return;
+//   }
+//   var buffer = new Buffer(100);
+//   fs.read(fd, buffer, 0, 100, 0, (err, num) => {
+//     console.dir(buffer.toString('utf8', 0, num));
+//   });
+// });
+const data$ = rxjs_1.fromEvent(fs.createReadStream('files/in/bin'), 'data').pipe(operators_1.tap(console.log));
+const bits$ = data$.pipe(operators_1.map(i => i.readInt8(0)));
+bits$.subscribe(console.log);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,34 @@
+import { from, fromEvent, bindNodeCallback } from 'rxjs';
+import { map, mergeMap, switchMap, tap } from 'rxjs/operators';
+
 import * as fs from 'fs';
 
-console.log(2 ** 4);
+// const bit$ = from([0, 0, 0, 1]);
+
+// bit$.pipe(map(i => !i)).subscribe(console.log);
+
+// const rs = fs.createReadStream('files/in/bin');
+
+// const data$ = fromEvent(rs, 'data').pipe(map((i: Buffer) => i.length));
+
+// data$.subscribe(console.log);
+
+// fs.open('files/in/bin', 'r', (error, fd) => {
+//   if (error) {
+//     console.error(error.message);
+//     return;
+//   }
+//   var buffer = new Buffer(100);
+//   fs.read(fd, buffer, 0, 100, 0, (err, num) => {
+//     console.dir(buffer.toString('utf8', 0, num));
+//   });
+// });
+
+const data$ = fromEvent<Buffer>(
+  fs.createReadStream('files/in/bin'),
+  'data'
+).pipe(tap(console.log));
+
+const bits$ = data$.pipe(map(i => i.readInt8(0)));
+
+bits$.subscribe(console.log);
