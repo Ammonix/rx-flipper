f66fd161d7e3ad3e54043bee867bc725ab955a6f
Joan Künzler <joan.kuenzler@raiffeisen.ch>
2018-04-27 16:43:53 +0200

    clean

--- dist/index.js
+++ dist/index.js
@@ -1,22 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const fs = require("fs");
 const rxjs_1 = require("rxjs");
 const operators_1 = require("rxjs/operators");
-const fs = require("fs");
 const bit_buffer_1 = require("bit-buffer");
-const data$ = rxjs_1.fromEvent(fs.createReadStream('files/in/bin'), 'data');
-const bits$ = data$.pipe(operators_1.map(i => new bit_buffer_1.BitStream(i)), operators_1.map(bs => {
-    const bits = [];
-    for (let i = 0; i < bs.length; i++) {
-        bits.push(bs.readBoolean());
+rxjs_1.fromEvent(fs.createReadStream('files/in/bin'), 'data')
+    .pipe(operators_1.map(i => new bit_buffer_1.BitStream(i)), operators_1.map(originalBS => {
+    const flippedBS = new bit_buffer_1.BitStream(new Buffer(originalBS.buffer.byteLength));
+    for (let i = 0; i < originalBS.length; i++) {
+        flippedBS.writeBoolean(!originalBS.readBoolean());
     }
-    return bits;
-}));
-const filpped$ = bits$.pipe(operators_1.map(i => i.map(b => !b)));
-filpped$.subscribe(i => {
-    const bs = new bit_buffer_1.BitStream(new ArrayBuffer(i.length / 8));
-    i.forEach(i => bs.writeBoolean(i));
+    return flippedBS;
+}))
+    .subscribe(i => {
     const ws = fs.createWriteStream('files/out/bin');
-    ws.write(bs.buffer);
+    ws.write(i.buffer);
     ws.end();
 });
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,21 @@
-import { from, fromEvent, bindNodeCallback, of } from 'rxjs';
-import { map, mergeMap, switchMap, tap } from 'rxjs/operators';
-
 import * as fs from 'fs';
-import { BitStream, BitView } from 'bit-buffer';
+import { fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { BitStream } from 'bit-buffer';
 
-const data$ = fromEvent<Buffer>(fs.createReadStream('files/in/bin'), 'data');
-
-const bits$ = data$.pipe(
-  map(i => new BitStream(i)),
-  map(bs => {
-    const bits = [];
-    for (let i = 0; i < bs.length; i++) {
-      bits.push(bs.readBoolean());
-    }
-    return bits;
-  })
-);
-
-const filpped$ = bits$.pipe(map(i => i.map(b => !b)));
-
-filpped$.subscribe(i => {
-  const bs = new BitStream(new ArrayBuffer(i.length / 8));
-  i.forEach(i => bs.writeBoolean(i));
-  const ws = fs.createWriteStream('files/out/bin');
-  ws.write(bs.buffer);
-  ws.end();
-});
+fromEvent<Buffer>(fs.createReadStream('files/in/bin'), 'data')
+  .pipe(
+    map(i => new BitStream(i)),
+    map(originalBS => {
+      const flippedBS = new BitStream(new Buffer(originalBS.buffer.byteLength));
+      for (let i = 0; i < originalBS.length; i++) {
+        flippedBS.writeBoolean(!originalBS.readBoolean());
+      }
+      return flippedBS;
+    })
+  )
+  .subscribe(i => {
+    const ws = fs.createWriteStream('files/out/bin');
+    ws.write(i.buffer);
+    ws.end();
+  });
